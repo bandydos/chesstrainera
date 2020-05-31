74e7090b2e7d9f606c75212f1a98e1161bfb11ec
KR44I <38465150+AlexaTack@users.noreply.github.com>
2020-05-31 18:05:02 +0200

    Filtered moves, added to table

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,90 @@
 $(document).ready(() => {
+    const btnStart = $('#btn-start');
     const btnOpen = $('#btn-open');
     const inputFile = $('#input-file');
+    const tableMoves = $('#table-moves');
     const pgnfile = inputFile[0].files[0];
+    tableMoves.hide();
     btnOpen.click(() => {
         inputFile.click();
     })
     inputFile.change(() => {
         console.log(pgnfile)
     })
-
-
-    $('#btn-start').click(() => {
+    btnStart.click(() => {
         readData();
-        splitMoves();
     })
 });
 
 const readData = () => {
-    const spanMoves = document.getElementById('span-moves');
-    if (isValid() == true ) {
-        spanMoves.textContent = "true";
+    const spanMessage = $('#span-message');
+    const tableMoves = $('#table-moves');
+    const moves = seperateMoves();
+    const valid = isValid();
+    if (valid == true) {
+        tableMoves.show();
+        spanMessage.hide();
+        for (let i = 0; i < moves.length; i++) {
+            let timeout = setTimeout(() => {
+                let counter = 0;
+                $('#tbody-moves').append(`<tr><td>${i + 1}</td><td>${moves[i]}</td></tr>`);
+
+                if (counter === moves.length) {
+                    clearTimeout(timeout);
+                }
+            }, 2000);
+            
+            
+        }
+
+
     } else {
-        spanMoves.textContent = "false"
+        tableMoves.hide();
+        spanMessage.show();
+        spanMoves.textContent = 'Something went wrong, check input please.';
     }
 }
 
 const isValid = () => {
-    const move = splitMoves();
-    // Start from move 1 (because of splitting by dots).
-    for(let i = 1; i < move.length; i++){
-        let onemove = move[i].split(' ');
-        onemove.pop();
-        console.log(onemove.join(' '));
-
+    const moves = seperateMoves();
+    let valid;
+    for (let i = 0; i < moves.length; i++) {
+        if (/^([NBRQK])?([a-h])?([1-8])?(x)?([a-h][1-8])(=[NBRQK])?(\+|#)?$|^O-O(-O)?$/.test(moves[i])) {
+            valid = true;
+        } else {
+            return false;
+        }
     }
-    if(/^([NBRQK])?([a-h])?([1-8])?(x)?([a-h][1-8])(=[NBRQK])?(\+|#)?$|^O-O(-O)?$/.test(move)){
-        return true;
-    } else {
-        return false;
-    }
+    return valid;
 }
 
-const splitMoves = () => {
+// Get moves per number e.g. 1.d4 d5 2.nf3 nc6
+// => [["d4", "d5"]["nf3", "nc6"]].
+const splittedPerMove = () => {
     const inputMoves = document.getElementById('input-moves').value;
     const splittedMoves = inputMoves.split('\.'); // Splitting by dots.
-    return splittedMoves;
+    let allmoves = [];
+    // Start from pos 1 (because of splitting by dots).
+    for (let i = 1; i < splittedMoves.length; i++) {
+        let onemove = splittedMoves[i].split(' ');
+        onemove.pop();
+        onemove.join(' ');
+        allmoves.push(onemove);
+    }
+    return allmoves;
 }
+
+// Get seperate moves e.g. 1.d4 d5 2.nf3 nc6
+// => ["d4", "d5", "nf3", "nc6"].
+const seperateMoves = () => {
+    const movesnr = splittedPerMove();
+    const seperateMoves = [];
+    for (let i = 0; i < movesnr.length; i++) {
+        for (let j = 0; j < movesnr[i].length; j++) {
+            seperateMoves.push(movesnr[i][j]);
+        }
+    }
+    return seperateMoves;
+}
+
 
