a02e5d6474a304cb2ec5277296a9c8457279136b
KR44I <38465150+AlexaTack@users.noreply.github.com>
2020-06-03 13:37:09 +0200

    Implemented better PGN parser

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ $(document).ready(() => {
     const tableMoves = $('#table-moves');
     const btnReset = $('#btn-reset');
 
-    const pgnfile = inputFile[0].files[0];
+    //const pgnfile = inputFile[0].files[0];
     tableMoves.hide();
     btnReset.hide();
 
@@ -16,63 +16,60 @@ $(document).ready(() => {
         console.log(pgnfile)
     })
     btnStart.click(() => {
+        console.log(parsedMoves())
         readData();
     })
     btnReset.click(() => {
-        clearTimeout(timer);
         $('#table-moves tbody tr').remove();
+        while(timers.length) clearTimeout(timers.shift())
         showElems(false);
     })
 });
-// let timer;
+
 
 const readData = () => {
     const spanMessage = $('#span-message');
-    const tableMoves = $('#table-moves');
-    const moves = seperateMoves();
+    const moves = parsedMoves();
     const valid = isValid();
     const intervalSpeed = getIntervalSpeed();
     showElems(valid);
-    clearTimeout(timer);
     $('#table-moves tbody tr').remove();
     if (valid === true) {
         for (let i = 0; i < moves.length; i++) {
             iterationLevel(i, intervalSpeed);
         }
     } else {
-        clearTimeout(timer);
         spanMessage.text('Something went wrong, check input please.');
     }
 }
 
-const speech = () => {
+// const speech = () => {
+//     let text = 'hello';
+//     let url = `https://translate.google.com/translate_tts?ie=UTF-8&tl=en-TR&client=tw-ob&q=${text}`;
+// }
 
-    let text = 'hello';
-    let url = `https://translate.google.com/translate_tts?ie=UTF-8&tl=en-TR&client=tw-ob&q=${text}`;
-}
+var timers = [];
 
-var timer;
-var iterator = 0;
 // Iterate over moves with delay.
 const iterationLevel = (i, interval) => {
-    const moves = seperateMoves();
-    timer = setTimeout(() => {
+    const moves = parsedMoves();
+    timers.push(setTimeout(() => {
         $('#tbody-moves').append(`<tr><td>${i + 1}</td><td>${getFull(moves[i])}</td></tr>`);
-
-        
-
-    }, interval * i);
+    }, interval * i));
     if (i >= moves.length) {
-        clearTimeout(timer);
+        clearTimeout(timers.pop());
         i = 0;
     }
 }
 
+// Validate.
 const isValid = () => {
-    const moves = seperateMoves();
+    const moves = parsedMoves();
     let valid;
+    const regmove = /^([NBRQK])?([a-h])?([1-8])?(x)?([a-h][1-8])(=[NBRQK])?(\+|#)?$|^O-O(-O)?$/;
+    const regscore = /(^0?\-1?$)|(^1?\-0?$)|(^1?\/2?\-1?\/2?$)/;
     for (let i = 0; i < moves.length; i++) {
-        if (/^([NBRQK])?([a-h])?([1-8])?(x)?([a-h][1-8])(=[NBRQK])?(\+|#)?$|^O-O(-O)?$/.test(moves[i])) {
+        if (regmove.test(moves[i]) || regscore.test(moves[i])) {
             valid = true;
         } else {
             return false;
@@ -81,6 +78,37 @@ const isValid = () => {
     return valid;
 }
 
+// Parse PGN file to array of moves.
+function parsedMoves() {
+    const full = $('#input-pgn').val();
+    const moves = full.substring(full.lastIndexOf(']') + 1);
+    const splitted = moves.split(/\s/);
+    const reg = /[0-9]+\./;
+    const filtered = splitted.filter((x) => {
+        return !reg.test(x) && x; // Filter out by regex + empty strings.
+    });
+    return filtered;
+}
+
+// Get interval speed.
+const getIntervalSpeed = () => {
+    const selectedVal = $('#select-level').val();
+    let interval;
+    if (selectedVal === 's') interval = 6000;
+    if (selectedVal === 'm') interval = 4000;
+    if (selectedVal === 'f') interval = 250;
+    return interval;
+}
+
+// Get full name.
+// Needs work => fix spaces befor words & double spaces.
+const getFull = (move) => {
+    return move.replace('N', ' knight ').replace('B', ' bishop ')
+    .replace('R', ' rook ').replace('Q', ' queen ').replace('K', ' king ')
+    .replace('x', ' takes ').replace('+', ' check ').replace('#', ' checkmate ');
+}
+
+// Specify elements to display when valid or invalid.
 const showElems = (valid) => {
     const spanMessage = $('#span-message');
     const tableMoves = $('#table-moves');
@@ -101,49 +129,4 @@ const showElems = (valid) => {
         tableMoves.hide();
         btnReset.hide();
     }
-}
-
-// Get moves per number e.g. 1.d4 d5 2.nf3 nc6
-// => [["d4", "d5"]["nf3", "nc6"]].
-const splittedPerMove = () => {
-    const inputMoves = document.getElementById('input-moves').value;
-    const splittedMoves = inputMoves.split('\.'); // Splitting by dots.
-    let allmoves = [];
-    // Start from pos 1 (because of splitting by dots).
-    for (let i = 1; i < splittedMoves.length; i++) {
-        let onemove = splittedMoves[i].split(' ');
-        onemove.pop();
-        onemove.join(' ');
-        allmoves.push(onemove);
-    }
-    return allmoves;
-}
-
-// Get seperate moves e.g. 1.d4 d5 2.nf3 nc6
-// => ["d4", "d5", "nf3", "nc6"].
-const seperateMoves = () => {
-    const movesnr = splittedPerMove();
-    const seperateMoves = [];
-    for (let i = 0; i < movesnr.length; i++) {
-        for (let j = 0; j < movesnr[i].length; j++) {
-            seperateMoves.push(movesnr[i][j]);
-        }
-    }
-    return seperateMoves;
-}
-
-// Get interval speed.
-const getIntervalSpeed = () => {
-    const selectedVal = $('#select-level').val();
-    let interval;
-    if (selectedVal === 's') interval = 6000;
-    if (selectedVal === 'm') interval = 4000;
-    if (selectedVal === 'f') interval = 1000;
-    return interval;
-}
-
-const getFull = (move) => {
-    return move.replace('N', ' knight ').replace('B', ' bishop ')
-    .replace('R', ' rook ').replace('Q', ' queen ').replace('K', ' king ')
-    .replace('x', ' takes ').replace('+', ' check ');
 }
