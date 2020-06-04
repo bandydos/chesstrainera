d37fff235b302d8fd4997b6a7a5d4021a011ad02
KR44I <38465150+AlexaTack@users.noreply.github.com>
2020-06-04 10:43:23 +0200

    Fixed parser and validation

--- scripts/script.js
+++ scripts/script.js
@@ -2,45 +2,42 @@ $(document).ready(() => {
     const btnStart = $('#btn-start');
     const btnOpen = $('#btn-open');
     const inputFile = $('#input-file');
-    const tableMoves = $('#table-moves');
     const btnReset = $('#btn-reset');
 
-    //const pgnfile = inputFile[0].files[0];
-    tableMoves.hide();
-    btnReset.hide();
-
+    showElems('stopped'); // Hide table ...
     btnOpen.click(() => {
         inputFile.click();
-    })
+    });
     inputFile.change(() => {
-        console.log(pgnfile)
-    })
+    });
+
     btnStart.click(() => {
-        console.log(parsedMoves())
         readData();
-    })
+    });
     btnReset.click(() => {
         $('#table-moves tbody tr').remove();
         clearTimeout(timeout);
-        showElems(false);
-    })
+        showElems('stopped');
+        $('#span-message').hide();
+    });
 });
 
-
 async function readData() {
-    const spanMessage = $('#span-message');
-    const moves = parsedMoves();
+    const moves = parsedSeperateMoves();
     const valid = isValid();
     const tempo = getTempo();
-    showElems(valid);
+
     $('#table-moves tbody tr').remove();
+
     if (valid === true) {
+        showElems('started');
         for (let i = 0; i < moves.length; i++) {
             $('#tbody-moves').append(`<tr><td>${i + 1}</td><td>${getFull(moves[i])}</td></tr>`);
             await delay(tempo);
         }
     } else {
-        spanMessage.text('Something went wrong, check input please.');
+        showElems('stopped');
+        $('#span-message').text('Something went wrong, check input please.');
     }
 }
 
@@ -53,29 +50,68 @@ function delay(ms) {
 
 // Validate.
 function isValid() {
-    const moves = parsedMoves();
+    const moves = parsedSeperateMoves();
     let valid;
-    const regmove = /^([NBRQK])?([a-h])?([1-8])?(x)?([a-h][1-8])(=[NBRQK])?(\+|#)?$|^O-O(-O)?$/;
-    const regscore = /^1-0$|^0-1$|^1\/2-1\/2$/;
-    for (let i = 0; i < moves.length; i++) {
-        if (regmove.test(moves[i]) || regscore.test(moves[i])) {
-            valid = true;
-        } else {
-            return false;
-        }
+    let validOutcome;
+    let validMove;
+
+    // Test moves.
+    const regMove = /^([NBRQK])?([a-h])?([1-8])?(x)?([a-h][1-8])(=[NBRQK])?(\+|#)?$|^O-O(-O)?$/;
+    for (let i = 0; i < moves.length - 1; i++) {
+        if (regMove.test(moves[i])) validMove = true;
+        else return false;
     }
+
+    // Test outcome.
+    const regOutcome = /^1-0$|^0-1$|^1\/2-1\/2$/;
+    if (regOutcome.test(moves[moves.length - 1])) validOutcome = true;
+    else return false;
+
+    // Combine.
+    if (validMove === true && validOutcome === true) valid = true;
+    else return false;
     return valid;
 }
 
+
 // Parse PGN text to array of moves.
-function parsedMoves() {
+function parsedFullMoves() {
     const full = $('#input-pgn').val();
-    const moves = full.substring(full.lastIndexOf(']') + 1);
-    const splitted = moves.split(/\s/); // Split per space.
+
+    const firstMove = /1\.([a-h]|[NBRQK])/ // Search first move.
+    const moves = full.substring(full.search(firstMove));
+
     const reg = /[0-9]+\./; // Numbering regex.
+    const splitted = moves.split(reg); // Split per move.
+
     const filtered = splitted.filter((x) => {
-        return !reg.test(x) && x; // Filter out numbering + empty strings.
+        return x; // Filter out empty strings.
     });
+
+    return filtered;
+}
+
+function parsedSeperateMoves() {
+    const fullMoves = parsedFullMoves();
+
+    // Split array further.
+    let tempMoves = [];
+    for (let i = 0; i < fullMoves.length; i++) {
+        tempMoves.push(fullMoves[i].split(/\s/));
+    }
+
+    // Store each element in new array.
+    let seperate = [];
+    for (let i = 0; i < tempMoves.length; i++) {
+        for (let j = 0; j < tempMoves[i].length; j++) {
+            seperate.push(tempMoves[i][j]);
+        }
+    }
+
+    let filtered = seperate.filter((x) => {
+        return x;  // Filter out empty strings.
+    });
+
     return filtered;
 }
 
@@ -92,26 +128,25 @@ function getTempo() {
 // Get full name.
 function getFull(move) {
     return move.replace('N', ' knight ').replace('B', ' bishop ')
-    .replace('R', ' rook ').replace('Q', ' queen ').replace('K', ' king ')
-    .replace('x', ' takes ').replace('+', ' check ').replace('#', ' checkmate ')
-    .replace(/\s+/g, ' '); // Single spaces.
+        .replace('R', ' rook ').replace('Q', ' queen ').replace('K', ' king ')
+        .replace('x', ' takes ').replace('+', ' check ').replace('#', ' checkmate ')
+        .replace(/\s+/g, ' '); // Single spaces.
 }
 
 // Specify elements to display when valid or invalid.
-function showElems(valid) {
+function showElems(action) {
     const spanMessage = $('#span-message');
     const tableMoves = $('#table-moves');
-    //const tableRows = $('#table-moves tbody tr'); 
     const divLabels = $('#div-labels');
     const divInput = $('#div-input');
     const btnReset = $('#btn-reset');
-    if (valid === true) {
+    if (action === 'started') {
         divLabels.hide();
         divInput.hide();
         spanMessage.hide();
         tableMoves.show();
         btnReset.show();
-    } else {
+    } else if (action === 'stopped') {
         divLabels.show();
         divInput.show();
         spanMessage.show();
